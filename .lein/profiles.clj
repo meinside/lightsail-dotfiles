9fbdfe4bc935472f6dd5fe9a0c41e8084549e80f
Sungjin Han <meinside@gmail.com>
2019-11-01 10:34:15 +0900

    Edit .gitignore and add .lein/profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,30 @@
+;; .lein/profiles.clj
+;;
+;; last update: 2019.11.01.
+
+{:user {
+        ;; plugins
+        :plugins [
+                  ;; https://github.com/kumarshantanu/lein-exec
+                  [lein-exec "0.3.7"]
+
+                  ;; https://github.com/technomancy/leiningen/tree/master/lein-pprint
+                  [lein-pprint "1.2.0"]
+
+                  ;; https://github.com/clojure-emacs/cider-nrepl
+                  [cider/cider-nrepl "0.21.1"]
+
+                  ;; https://github.com/venantius/ultra
+                  ;;[venantius/ultra "0.6.0"]
+
+                  ;; https://github.com/xsc/lein-ancient
+                  [lein-ancient "0.6.15"]]
+
+        ;; dependencies
+        :dependencies [
+                       ;; https://github.com/technomancy/slamhound
+                       [slamhound "1.5.5"]]
+
+        ;; global variables
+        :global-vars {*print-length* 20}}}
+
